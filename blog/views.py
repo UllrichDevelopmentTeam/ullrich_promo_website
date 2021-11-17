640a7a29e0c0a7a50b6c4d9b39a9bd3a04101f7e
Morthais <girobyte98@gmail.com>
2021-11-17 13:35:42 -0700

    minor changes

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,5 @@
-"""
-Request and pass information from a model (ex. Post) to a template.
-These views are Python functions with blog logic.
-"""
-
 from django.shortcuts import render # function to put-together templates
-from .models import Post # model to pull information
+from .models import Post # include the Post model to pull data
 from django.utils import timezone 
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
@@ -12,17 +7,24 @@ from django.shortcuts import redirect
 
 def post_list(request):
     """
-    Show a list of posts.
+    Generate a list of posts.
     """
-    # posts is a Queryset of all Posts
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # render() provides post_list.html with the list of posts as posts
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
+    """
+    Get the requested post.
+    Render using the post_detail template.
+    """
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    """
+    Create a new post.
+    """
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
