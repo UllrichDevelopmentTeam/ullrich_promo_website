b2ae3e951de01608cb32bc937c78e0f9c457a261
Morthais <girobyte98@gmail.com>
2021-11-19 13:42:26 -0700

    change the homepage

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # Landing page path (the empty path '' redirects to the views.post_list module) is a list of posts
     # What do we want the landing page to be for Colorado Family Insurance?
-    path('', views.post_list, name='post_list'),
+    path('', views.index, name='index'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
