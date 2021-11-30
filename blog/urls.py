1b67f2f7c34ca0c7ce22ec3e384707655999c8ba
Morthais <girobyte98@gmail.com>
2021-11-30 16:13:56 -0700

    refactor source code

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,4 @@ urlpatterns = [
     # Landing page path (the empty path '' redirects to the views.post_list module) is a list of posts
     # What do we want the landing page to be for Colorado Family Insurance?
     path('', views.index, name='index'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
-    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,68 +1,11 @@
-from django.shortcuts import render # function to put-together templates
-from .models import Post # include the Post model to pull data
+# function to put-together templates depending on the browser request
+from django.shortcuts import render 
+
+# other imports that may be useful
 from django.utils import timezone 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
-
-def post_list(request):
-    """
-    Generate a list of posts.
-    """
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    # render() provides post_list.html with the list of posts as posts
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    """
-    Get the requested post.
-    Render using the post_detail template.
-    """
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-    """
-    Create a new post.
-    """
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:    
-        form = PostForm()
-
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
-def post_publish(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('post_detail', pk=pk)
-
-def post_remove(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
 
 def index(request):
     return render(request, 'blog/index.html')
