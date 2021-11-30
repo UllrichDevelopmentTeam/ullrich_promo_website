09f8fa58887956c9a98dd67e6a1cccd0ff7e8f56
Morthais <girobyte98@gmail.com>
2021-11-30 16:32:33 -0700

    link blog app with index

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Landing page path (the empty path '' redirects to the views.post_list module) is a list of posts
     # What do we want the landing page to be for Colorado Family Insurance?
     path('', views.index, name='index'),
+    path('post_list/', views.post_list, name="post_list"),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
